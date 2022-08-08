61272aebcc997a0d8ab0ed1eb0c72801734537a2
Jfrancks <jloaisigaflores2g@gmail.com>
2022-08-07 23:08:48 -0600

    Agregar archivos de proyecto.

---
+++ WebAppLafise/Models/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppLafise.Models
+{
+    [Table("Libro")]
+    public partial class Libro
+    {
+        
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string? Titulo { get; set; }
+        [Column("Id_Autor")]
+        public int? IdAutor { get; set; }
+    }
+}
